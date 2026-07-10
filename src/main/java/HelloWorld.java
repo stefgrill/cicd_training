5cfe5b267798b2ba4613b1b42e339c9bb9a72683
stefgrill <steffen.grill@vector.com>
2026-07-10 13:55:59 +0200

    Updated HelloWorld text output

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hallo Welt");
+        System.out.println("Hallo Welt ich bin automatisiert in einer Pipeline gebaut und hochgeladen");
     }
 }
